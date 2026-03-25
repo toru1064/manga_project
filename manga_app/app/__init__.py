e8be3372bcf5a39d38ef3b160e8f404a44a07cfd
とーる <kna.metaru@gmail.com>
2026-03-26 02:16:38 +0900

    Render用に初期テーブル作成処理を追加

--- manga_app/app/__init__.py
+++ manga_app/app/__init__.py
@@ -26,5 +26,8 @@ def create_app():
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
+    
+    with app.app_context():
+        db.create_all()
 
     return app
