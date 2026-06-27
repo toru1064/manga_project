eb2e502b3fb7d23c56b6af6e12d30e5c705b2966
とーる <kna.metaru@gmail.com>
2026-06-27 21:13:45 +0900

    AWSデプロイ準備

--- manga_app/app/__init__.py
+++ manga_app/app/__init__.py
@@ -26,9 +26,5 @@ def create_app():
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
-    
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
 
     return app
